d6253da2817a61ae55f76a5d4d8a887a80f37523
ars899 <arsenkadaev@mail.ru>
2019-03-17 17:11:19 +0300

    Калькулятор - столбцы стоят рядом

--- src/com/company/MathApp.java
+++ src/com/company/MathApp.java
@@ -2,30 +2,15 @@ package com.company;
 
 public class MathApp {
     void math() {
+        int i,j;
+        for (i=0;i<10;i++) {
+            for (j=0;j<10;j++) {
+                if (i*j == 0){
 
-         for (int j=1;j<10;j++) {
-
-             int i= 1;
-                int one =(i+1);
-                int two =(i+2);
-                int three =(i+3);
-                int four =(i+4);
-                int five =(i+5);
-                int six =(i+6);
-                int seven =(i+7);
-                int eight =(i+8);
-                int nine =(i+9);
-                System.out.println(i + "*" + j + "=" + (i*j) + " "
-                        + one + "*" + j + "=" + (one * j) + " "
-                        + two + "*" + j + "=" + (two * j) + " "
-                        + three + "*" + j + "=" + (three * j) + " "
-                        + four + "*" + j + "=" + (four * j) + " "
-                        + five + "*" + j + "=" + (five * j) + " "
-                        + six + "*" + j + "=" + (six * j) + " "
-                        + seven + "*" + j + "=" + (seven * j) + " "
-                        + eight + "*" + j + "=" + (eight * j) + " "
-                        + nine + "*" + j + "=" + (nine * j));
-
+                } else
+                System.out.print(j + "*" + i + "=" + (i*j) + "\t");
+            }
+            System.out.println(" ");
         }
     }
 }
